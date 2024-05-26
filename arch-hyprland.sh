859705f54408dd5c5336d46b09d6f03c6554f342
itwars <vincent.rabah@gmail.com>
2024-05-26 18:27:41 +0200

    add archlinux hyprland

---
+++ arch-hyprland.sh
@@ -0,0 +1,15 @@
+sudo pacman -Syu
+sudo pacman -S hyprland \
+                plasma \
+                kitty \
+                neovim \
+                mc \
+                waybar \
+                mako \
+                pipewire \
+                wireplumber \
+                wofi \
+                firefox \
+                ttf-noto-nerd \
+                fastfetch \
+                xdg-desktop-portal-hyprland
